c8ac246d2d81974186d94982e6cf30efa962f851
mateusz2202 <jmate2202@outlook.com>
2022-03-05 20:47:41 +0100

    add migration

--- API-SHOP/Data/ShopContext.cs
+++ API-SHOP/Data/ShopContext.cs
@@ -5,7 +5,16 @@ namespace API_SHOP.Data
 {
     public class ShopContext : DbContext
     {
+        public ShopContext(DbContextOptions options) : base(options)
+        {
+        }
 
-        public DbSet<Product> Products { get; set; } = null;
+        public DbSet<Product> Products { get; set; }
+        public DbSet<WareHouse> WareHouses { get; set; }
+        public DbSet<InfoProduct> InfoProducts { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ShoppingBasket> ShoppingBaskets { get; set; }
+        public DbSet<InfoBoughtProduct> InfoBoughtProducts { get; set; }
     }
 }
---
+++ API-SHOP/Migrations/20220305193209_InitialCreate.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_SHOP.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<int>(type: "int", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WareHouse",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WareHouse", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InfoProduct",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    WareHouseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InfoProduct", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InfoProduct_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_InfoProduct_WareHouse_WareHouseId",
+                        column: x => x.WareHouseId,
+                        principalTable: "WareHouse",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfoProduct_ProductId",
+                table: "InfoProduct",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfoProduct_WareHouseId",
+                table: "InfoProduct",
+                column: "WareHouseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InfoProduct");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "WareHouse");
+        }
+    }
+}

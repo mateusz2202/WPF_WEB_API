172ba0ece6859f5e9a04c5928464c6eecf0147eb
mateusz2202 <jmate2202@outlook.com>
2022-03-05 01:48:15 +0100

    configurate connection to data base

---
+++ API-SHOP/Data/ShopContext.cs
@@ -0,0 +1,11 @@
+﻿using API_SHOP.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_SHOP.Data
+{
+    public class ShopContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; } = null;
+    }
+}
--- API-SHOP/Program.cs
+++ API-SHOP/Program.cs
@@ -1,8 +1,12 @@
+using API_SHOP.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ShopContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Shop_DB_Connection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

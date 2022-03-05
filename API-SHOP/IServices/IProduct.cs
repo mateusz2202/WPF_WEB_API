9569cf4d8e51bf37d84f2371bb1a1df7c7573a66
mateusz2202 <jmate2202@outlook.com>
2022-03-05 01:02:16 +0100

    Create Interface

---
+++ API-SHOP/IServices/IProduct.cs
@@ -0,0 +1,13 @@
+﻿using API_SHOP.Entities;
+
+namespace API_SHOP.IServices
+{
+    public interface IProduct
+    {
+        List<Product>GetAll();
+        Product GetById(int id);
+        void CreateProduct(Product product);
+        void UpdateProduct(int id,Product product);
+        void DeleteProduct(int id);
+    }
+}
